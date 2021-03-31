a516fe05b65445d7389b427480d56d170ea1ae30
Usarneme <jamestcoop@gmail.com>
2021-03-31 14:21:06 -0700

    updates the readme to include the first test. it updates the scripts.js file to include helper functions for isVowel and isFirstLetterVowel booleans. it updates pigLatin() to pass the first test adding way.
    Co-authored-by: Shane Graff <copellius@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
-function pigLatin() {
-  return "ok!"
+// Helper Functions
+function isVowel(letter) {
+  const vowels = ["a","e","i","o","u"];
+  if (vowels.includes(letter)) {
+    return true
+  } else {
+    return false
+  }
 }
+
+function isFirstLetterVowel(word) {
+  if (isVowel(word[0])) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+// Business Logic
+function pigLatin(phrase) {
+  let pigLatinPhrase = "";
+
+  if (isVowel(phrase)) {
+    pigLatinPhrase = phrase.concat("way");
+  }
+
+  return pigLatinPhrase;
+}
+
+
+
+
+
+// UI Logic
+
+
+
