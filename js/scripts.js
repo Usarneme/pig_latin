27a63b3f5409c550e92026a9569fd7469b76cb95
Usarneme <jamestcoop@gmail.com>
2021-03-31 15:16:42 -0700

    passes the third test in our spec. moving all consecutive consonants to the end of the phrase input.
    Co-authored-by: Shane Graff <copellius@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,24 +18,40 @@ function isFirstLetterVowel(word) {
 
 // Business Logic
 
-// "ba"
+// "bbba"
 function pigLatin(phrase) {
   let pigLatinPhrase = "";
 
-  // isFirstLetterVowel"ba") => false
   if (isFirstLetterVowel(phrase)) {
     pigLatinPhrase = phrase.concat("way");
   } else {
-    pigLatinPhrase = phrase.slice(1);
-    // pigLatinPhrase = "a"
-    pigLatinPhrase += phrase.slice(0,1)
-    // pigLatinPhrase = "ab"
+    let indexOfFirstVowel;
+
+    // phrase = "bbba"
+    for (let index = 0; index < phrase.length; index += 1) {
+      // if the letter at this index in the prhase is a vowel,
+      // then we update the indexOfFirstVowel variable
+      // index = 0, "bbba"[0] = "b"
+      // index = 1, "bbba"[1] = "b"
+      // ..
+      // index = 3, "bbba[3] = "a" DING DING DING!
+      if (isVowel(phrase[index])) {
+        indexOfFirstVowel = index;
+        break;
+      }
+    }
+    // now we know where the first vowel is in the phrase
+
+    // store all of the phrase AFTER the first vowel
+    pigLatinPhrase = phrase.slice(indexOfFirstVowel);
+    // append/tack on the rest of the phrase
+    pigLatinPhrase += phrase.slice(0, indexOfFirstVowel);
   }
 
   return pigLatinPhrase;
 }
 
-
+// "bbba" => "abbb"
 
 
 // UI Logic
